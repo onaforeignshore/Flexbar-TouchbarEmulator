c9a6adb3078ea7c1decdabdf3ae9a45b44f56294
Christo Kotze <onaforeignshore@hotmail.co.uk>
2025-09-13 15:30:42 +0400

    Create emulator.vue

---
+++ com.chino.touchbaremulator.plugin/ui/emulator.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <div class="mx-2">Touch Bar Emulator</div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    // FlexDesigner injects the key’s model (style/data/etc.) here.
+    modelValue: { type: Object, required: true },
+  },
+  emits: ['update:modelValue'],
+  mounted() {
+    // Optional: sanity ping
+    this.$fd?.info?.('TouchbarEmulator UI mounted');
+  },
+};
+</script>
+
+<style scoped></style>
